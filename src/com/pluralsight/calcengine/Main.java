f5ae534cfb53aa4b47dd68b72ff1fa221ed1f309
David Quigley <daquigley4@gmail.com>
2017-11-25 15:44:59 -0500

    added inheritance.

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -63,6 +63,23 @@ public class Main {
         System.out.println("results = " + equationOverload.getResult());
         System.out.println();
 
+        System.out.println();
+        System.out.println("Using Inheritance");
+        System.out.println();
+
+        CalculateBase[] calculators = {
+                new Divider(100.0d, 50.0d),
+                new Adder(25.0d, 92.0d),
+                new Subtractor(225.0d, 17.0d),
+                new Multiplier(11.0d, 3.0d)
+        };
+
+        for(CalculateBase calculator : calculators){
+            calculator.calculate();
+            double result = calculator.getResult();
+            System.out.println("Inheritance result = " + result);
+        }
+
 
     }
 

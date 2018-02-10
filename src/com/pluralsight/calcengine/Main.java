a6c76c10f943bf33c050198103acb6b7e3c43249
David Quigley <daquigley4@gmail.com>
2018-02-10 17:27:05 -0500

    added helper.

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -10,20 +10,37 @@ public class Main {
     }
 
     public static void main(String[] args) {
+//    useMathEquation();
+//    useCalculateBase();
 //        double[] leftVal = new double[]{100.0d, 25.0d, 225.0d, 11.0d};
 //        double[] rightVal = new double[]{50.0d, 92.0d, 17.0d, 3.0d};
 //        char[] opCodes = new char[]{'d', 'a', 's', 'm'};
 //        double[] results = new double[opCodes.length];
 
-        MathEquation testEquation = new MathEquation();
-        testEquation.execute();
-        System.out.println(testEquation.getResult());
+        String[] statements = {
+                "add 1.0",
+                "add xx 25.0",
+                "addX 0.0 0.0",
+                "divide 100.0d 25.0d",
+                "add 25.0d 92.0d",
+                "subtract 225.0d 17.0d",
+                "multiply 11.0d 3.0d"
+        };
 
+        CalculateHelper helper = new CalculateHelper();
+        for (String statement : statements) {
+            helper.process(statement);
+            System.out.println(helper);
+        }
+    }
+
+    static void useMathEquation() {
         MathEquation[] equations = new MathEquation[4];
         equations[0] = new MathEquation('d', 100.0d, 50.0d);
         equations[1] = new MathEquation('a', 25.0d, 92.0d);
         equations[2] = new MathEquation('s', 225.0d, 17.0d);
         equations[3] = new MathEquation('m', 11.0d, 3.0d);
+
 //        equations[0] = create(100.0d, 50.0d, 'd');
 //        equations[1] = create(25.0d, 92.0d, 'a');
 //        equations[2] = create(225.0d, 17.0d, 's');
@@ -63,6 +80,9 @@ public class Main {
         System.out.println("results = " + equationOverload.getResult());
         System.out.println();
 
+    }
+
+    static void useCalculateBase() {
         System.out.println();
         System.out.println("Using Inheritance");
         System.out.println();
@@ -74,13 +94,11 @@ public class Main {
                 new Multiplier(11.0d, 3.0d)
         };
 
-        for(CalculateBase calculator : calculators){
+        for (CalculateBase calculator : calculators) {
             calculator.calculate();
             double result = calculator.getResult();
             System.out.println("Inheritance result = " + result);
         }
-
-
     }
 
     public static MathEquation create(double leftVal, double rightVal, char opCode) {
